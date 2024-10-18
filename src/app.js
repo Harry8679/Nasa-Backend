e6571c0519bf4ddb8b4b803aeb710cf32ce53a48
Harry EMARH <emarh.harry.code@gmail.com>
2024-10-18 22:30:05 +0200

    kepler planets data and js code added

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const app = express();
 const planetsRoutes = require('./routes/planets/planets.route');
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3030'
+}));
 app.use(express.json());
 app.use('/api/v1/planets', planetsRoutes);
 
