287290038f3675633c5d1d0c2be7909bacdc8763
Harry EMARH <emarh.harry.code@gmail.com>
2024-11-18 22:59:48 +0100

    Redirect index.html to the homepage

--- src/app.js
+++ src/app.js
@@ -10,5 +10,8 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/api/v1/planets', planetsRoutes);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+});
 
 module.exports = app;
