7bac093c4743b47151fb341b4044b1dc9306caf2
Harry EMARH <emarh.harry.code@gmail.com>
2024-11-18 22:46:01 +0100

    static join public folder

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const planetsRoutes = require('./routes/planets/planets.route');
 
@@ -7,6 +8,7 @@ app.use(cors({
     origin: 'http://localhost:3030'
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/api/v1/planets', planetsRoutes);
 
 module.exports = app;
