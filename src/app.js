151818eeb78e449417f20c859a1eafcfd8099f8a
Harry EMARH <emarh.harry.code@gmail.com>
2024-11-19 17:24:35 +0100

    mogan middleware added in app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
+const morgan = require('morgan');
 const planetsRoutes = require('./routes/planets/planets.route');
 
 app.use(cors({
     origin: 'http://localhost:3030'
 }));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/api/v1/planets', planetsRoutes);
