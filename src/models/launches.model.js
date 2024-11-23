3f80aa2bfc19b9a87b5fa243d9634fe87af4d47e
Harry EMARH <emarh.harry.code@gmail.com>
2024-11-23 21:32:03 +0100

    Get All Launches

--- src/models/launches.model.js
+++ src/models/launches.model.js
@@ -13,4 +13,9 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
-module.exports = { launches };
+function getAllLaunches() {
+    return Array.from(launches.values());
+}
+
+
+module.exports = { getAllLaunches };
