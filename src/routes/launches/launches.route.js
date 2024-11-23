984fa52497b0e9d9c588d70042d0a44d33dcc5d4
Harry EMARH <emarh.harry.code@gmail.com>
2024-11-23 20:58:29 +0100

    Useless comments

--- src/routes/launches/launches.route.js
+++ src/routes/launches/launches.route.js
@@ -5,19 +5,5 @@ const { getAllLaunches } = require('../../controllers/launches/launches.controll
 const launchesRouter = express.Router();
 
 launchesRouter.get('/', getAllLaunches);
-// launchesRouter.get('/', (req, res) => {
-//     res.send('Launches');
-// });
 
 module.exports = launchesRouter;
-
-/*
-const express = require('express');
-const { getAllPlanets } = require('../../controllers/planets/planets.controller');
-
-const planetsRouter = express.Router();
-
-planetsRouter.get('/', getAllPlanets);
-
-module.exports = planetsRouter;
-*/
