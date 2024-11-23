292d9f2ec03050c35a3810531e9182611b56abdc
Harry EMARH <emarh.harry.code@gmail.com>
2024-11-23 20:18:30 +0100

    launches controller file created

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/api/v1/planets', planetsRoutes);
 app.use('/api/v1/launches', lauchesRoutes);
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 });
