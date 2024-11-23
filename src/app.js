15a68c28314667dba70166a0a65d9257e0ccddf2
Harry EMARH <emarh.harry.code@gmail.com>
2024-11-23 20:57:44 +0100

    Get All Launches

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const path = require('path');
 const app = express();
 const morgan = require('morgan');
 const planetsRoutes = require('./routes/planets/planets.route');
-const lauchesRoutes = require('./routes/launches/launches.route');
+const launchesRoutes = require('./routes/launches/launches.route');
+// const launchesRoutes = require('./routes/launches/launches.route');
 
 app.use(cors({
     origin: 'http://localhost:3030'
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/api/v1/planets', planetsRoutes);
-app.use('/api/v1/launches', lauchesRoutes);
+app.use('/api/v1/launches', launchesRoutes);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
