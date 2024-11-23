083f4b40e7ecb58bb1730f44fe113a5ef402f929
Harry EMARH <emarh.harry.code@gmail.com>
2024-11-23 20:31:26 +0100

    getAllLaunches imported in launches route

--- src/routes/launches/launches.route.js
+++ src/routes/launches/launches.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { launches } = require('../../models/launches.model');
+const { getAllLaunches } = require('../../controllers/launches/launches.controller');
 
 const launchesRouter = express.Router();
 
